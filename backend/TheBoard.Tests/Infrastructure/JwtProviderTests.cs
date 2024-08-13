9f45d916c3e3c99082644a7869691846d9ed9d78
Naklen <dryagin.ser@yandex.ru>
2024-08-13 16:00:59 +0500

    test(auth): fixed jwtprovider test to new implementation

--- backend/TheBoard.Tests/Infrastructure/JwtProviderTests.cs
+++ backend/TheBoard.Tests/Infrastructure/JwtProviderTests.cs
@@ -1,4 +1,5 @@
-﻿using TheBoard.Infrastructure.Auth;
+﻿using TheBoard.Application.Contracts;
+using TheBoard.Infrastructure.Auth;
 
 namespace TheBoard.Tests.Infrastructure;
 
@@ -11,10 +12,10 @@ public class JwtProviderTests
     {
         var jwtProvider = new JwtProvider();
         var secretKey = "4S9BasU9Y9AchV2LaxvasITakPnwVyZ1";
-        var payload = new (string, string)[]
+        var payload = new TokenPayloadField[]
         {
-            ("UserId", "megaUser"),
-            ("SessionId", "curentSessionId")
+            new ("UserId", "megaUser"),
+            new ("SessionId", "curentSessionId")
         };
         var expireIn = DateTime.Parse("3000-09-29T00:00:00.0000000+05:00");
         var expectedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOiJtZWdhVXNlciIsIlNlc3Npb25JZCI6ImN1cmVudFNlc3Npb25JZCIsImV4cCI6MzI1MjcwNzY0MDB9.kMM_-WfZPevJeQVAy6WRd2AQLHhu4uN4_5C8lPIBxMQ";
@@ -31,13 +32,13 @@ public class JwtProviderTests
         var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOiIyOTk1OWQ4NS04MTllLTRiNDEtYmY0Yi01ZDk1YzVjZTg1ZGQiLCJleHAiOjc4Mjc5NTY0MDN9.LN__MbM5sHK6-uOflHqfGzTV7MMXQ3NcS7LftlEPcKY";
         var secretKey = "MYsecretKey123secretk3asedgsedgsdgY";
 
-        var verified = jwtProvider.VerifyToken(token, secretKey, out (string, string)[] payload);
+        var verified = jwtProvider.VerifyToken(token, secretKey, out IEnumerable<TokenPayloadField> payload);
 
         Assert.True(verified);
         Assert.NotNull(payload);
         Assert.NotEmpty(payload);
-        Assert.Equal("UserId", payload[0].Item1);
-        Assert.Equal("29959d85-819e-4b41-bf4b-5d95c5ce85dd", payload[0].Item2);
+        Assert.Equal("UserId", payload.First().Key);
+        Assert.Equal("29959d85-819e-4b41-bf4b-5d95c5ce85dd", payload.First().Value);
     }
 
     [Fact]
@@ -47,7 +48,7 @@ public class JwtProviderTests
         var expiredToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOiIyOTk1OWQ4NS04MTllLTRiNDEtYmY0Yi01ZDk1YzVjZTg1ZGQiLCJleHAiOjE2NzI1NTY0MDB9.-8hskryeR84n8cpzUACMSsfwi6h6PpzDs4HbnuIZJqc";
         var secretKey = "lw4arXKWh8prVc6ewygQIPBKjBEvTfnP";
 
-        var verified = jwtProvider.VerifyToken(expiredToken, secretKey, out (string, string)[] payload);
+        var verified = jwtProvider.VerifyToken(expiredToken, secretKey, out IEnumerable<TokenPayloadField> payload);
 
         Assert.False(verified);
         Assert.Null(payload);
@@ -60,7 +61,7 @@ public class JwtProviderTests
         var changedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOiIyOTk1OWQ4NS04MTllLTRiNDEtYmY0Yi01ZDk1YzVjZTg1ZGQiLCJleHAiOjczMjI5NDY1MjAwfQ.imGdCJPoV-s0ZUjzo1wDAQezMNdDcVZRwypv5zhgTFs";
         var secretKey = "lw4arXKWh8prVc6ewygQIPBKjBEvTfnP";
 
-        var verified = jwtProvider.VerifyToken(changedToken, secretKey, out (string, string)[] payload);
+        var verified = jwtProvider.VerifyToken(changedToken, secretKey, out IEnumerable<TokenPayloadField> payload);
 
         Assert.False(verified);
         Assert.Null(payload);
@@ -73,7 +74,7 @@ public class JwtProviderTests
         var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOiJ1c2VySWQiLCJleHAiOjQ4MjgyMTU5NjAwfQ.7htlmhBfDFE1X0-V49ChDGxb3PZ6ynjGKoCpReL-s60";
         var wrongSecretKey = "wrongsecretkey";
 
-        var verified = jwtProvider.VerifyToken(token, wrongSecretKey, out (string, string)[] payload);
+        var verified = jwtProvider.VerifyToken(token, wrongSecretKey, out IEnumerable<TokenPayloadField> payload);
 
         Assert.False(verified);
         Assert.Null(payload);
